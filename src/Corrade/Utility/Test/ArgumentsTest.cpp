71169d57f8881757b99b9b84f801c248fd87d492
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 01:45:41 +0200

    uhh

--- src/Corrade/Utility/Test/ArgumentsTest.cpp
+++ src/Corrade/Utility/Test/ArgumentsTest.cpp
@@ -16,9 +16,8 @@ template<class T> struct Color3: Vec3<T> {
     constexpr Color3(T r, T g, T b) noexcept: Vec3<T>{r, g, b} {}
 };
 
-template<class T> struct Color4 {
-    constexpr Color4(const Vec3<T>& rgb, T a = someFuncThatReturnsOne<T>()) noexcept: d{rgb.d[0], rgb.d[1], rgb.d[2], a} {}
-    T d[4];
+template<class T> struct Color4: Vec<4, T> {
+    constexpr Color4(const Vec<3, T>& rgb, T a = someFuncThatReturnsOne<T>()) noexcept: Vec<4, T>{rgb.d[0], rgb.d[1], rgb.d[2], a} {}
 };
 
 constexpr float foo(const Color4<float>& c) { return c.d[3]; }
