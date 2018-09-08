4317ff159a8311e2ad6f8fa32da281806ae606c8
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 01:42:36 +0200

    fdljgugh

--- src/Corrade/Utility/Test/ArgumentsTest.cpp
+++ src/Corrade/Utility/Test/ArgumentsTest.cpp
@@ -3,14 +3,13 @@ template<class T> T someFuncThatReturnsOne();
 template<> constexpr unsigned char someFuncThatReturnsOne<unsigned char>() { return 255; }
 template<> constexpr float someFuncThatReturnsOne<float>() { return 1.0f; }
 
-// template<int size, class T> struct Vec {
-//     template<class ...Args> constexpr Vec(Args... args) noexcept: d{args...} {}
-//     T d[3];
-// };
+template<int size, class T> struct Vec {
+    template<class ...Args> constexpr Vec(Args... args) noexcept: d{args...} {}
+    T d[size];
+};
 
-template<class T> struct Vec3 {
-    constexpr Vec3(T r, T g, T b) noexcept: d{r, g, b} {}
-    T d[3];
+template<class T> struct Vec3: Vec<3, T> {
+    constexpr Vec3(T r, T g, T b) noexcept: Vec<3, T>{r, g, b} {}
 };
 
 template<class T> struct Color3: Vec3<T> {
@@ -22,9 +21,9 @@ template<class T> struct Color4 {
     T d[4];
 };
 
-constexpr float foo(const Color4<float>& c) { return c.d[0]; }
+constexpr float foo(const Color4<float>& c) { return c.d[3]; }
 
 int main() {
-    constexpr float a = foo(Color3<float>(0, 0, 0));
+    constexpr float a = foo(Color3<float>(2.0f, 1.0f, 0.3f));
     return a;
 }
