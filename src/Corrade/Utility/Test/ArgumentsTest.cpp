a8acad73bc7819f686a205449745b06c7c1d4e21
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 02:54:42 +0200

    LET'S GO RADICAL

--- src/Corrade/Utility/Test/ArgumentsTest.cpp
+++ src/Corrade/Utility/Test/ArgumentsTest.cpp
@@ -12,34 +12,11 @@ template<class T, unsigned int bits = sizeof(T)*8> inline constexpr T bitMax() {
 template<class T> constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type someFuncThatReturnsOne() {
     return T(1);
 }
-template<class T> constexpr typename std::enable_if<std::is_integral<T>::value, T>::type someFuncThatReturnsOne() {
-    return Implementation::bitMax<T>();
-}
+// template<class T> constexpr typename std::enable_if<std::is_integral<T>::value, T>::type someFuncThatReturnsOne() {
+//     return Implementation::bitMax<T>();
+// }
 
 }
-
-
-// template<> constexpr float someFuncThatReturnsOne<float>() { return 1.0f; }
-
-template<int size, class T> struct Vec {
-    template<class ...Args> constexpr Vec(Args... args) noexcept: d{args...} {}
-    T d[size];
-
-    T& operator[](int pos) { return d[pos]; }
-    constexpr T operator[](int pos) const { return d[pos]; }
-};
-
-template<class T> struct Vec3: Vec<3, T> {
-    constexpr Vec3(T r, T g, T b) noexcept: Vec<3, T>{r, g, b} {}
-};
-
-template<class T> struct Vec4: Vec<4, T> {
-    constexpr Vec4(T r, T g, T b, T a) noexcept: Vec<4, T>{r, g, b, a} {}
-};
-
-template<class T> struct Color3: Vec3<T> {
-    constexpr Color3(T r, T g, T b) noexcept: Vec3<T>{r, g, b} {}
-};
 
 template<class T> struct Color4 {
     constexpr Color4(T a, T b = Implementation::someFuncThatReturnsOne<T>()) noexcept: d{a, b} {}
