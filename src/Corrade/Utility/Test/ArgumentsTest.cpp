4579b43e870366de261e4c0cf22eb469c183c2e4
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 02:22:40 +0200

    :(

--- src/Corrade/Utility/Test/ArgumentsTest.cpp
+++ src/Corrade/Utility/Test/ArgumentsTest.cpp
@@ -1,9 +1,19 @@
 #include <type_traits>
 
+namespace Implementation {
+
+template<class T, unsigned int bits = sizeof(T)*8> inline constexpr T bitMax() {
+    return T(typename std::make_unsigned<T>::type(~T{}) >> (sizeof(T)*8 - (std::is_signed<T>::value ? bits - 1 : bits)));
+}
+
+}
+
 // template<class T> T someFuncThatReturnsOne();
 //
 // template<> constexpr unsigned char someFuncThatReturnsOne<unsigned char>() { return 255; }
-
+template<class T> constexpr typename std::enable_if<std::is_integral<T>::value, T>::type someFuncThatReturnsOne() {
+    return Implementation::bitMax<T>();
+}
 template<class T> constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type someFuncThatReturnsOne() {
     return T(1);
 }
