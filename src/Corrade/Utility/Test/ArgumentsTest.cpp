41b93a6107af3e19a74d5c04684f941cde74b370
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 03:27:39 +0200

    dfjh

--- src/Corrade/Utility/Test/ArgumentsTest.cpp
+++ src/Corrade/Utility/Test/ArgumentsTest.cpp
@@ -1,17 +1,17 @@
 #include <type_traits>
 
-namespace Implementation {
+namespace detail {
 
-template<int a> constexpr typename std::enable_if<a == 1, int>::type yay() {
+template<int a> constexpr typename std::enable_if<a == 1, bool>::type byte() {
     return 1;
 }
-template<int a> constexpr typename std::enable_if<a != 1, int>::type yay() {
+template<int a> constexpr typename std::enable_if<a != 1, bool>::type byte() {
     return 0;
 }
 
 }
 
-template<class T> constexpr T foo(T, T = Implementation::yay<sizeof(T)>()) { return {}; }
+template<class T> constexpr T foo(T, bool = detail::byte<sizeof(T)>()) { return {}; }
 
 int main() {
     foo(1.0f);
