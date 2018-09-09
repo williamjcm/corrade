56476d44ad1bdee925d1643c0c72f71cc02c5b11
Vladimír Vondruš <mosra@centrum.cz>
2018-09-09 04:00:58 +0200

    HA

--- package/ci/appveyor-desktop.bat
+++ package/ci/appveyor-desktop.bat
@@ -6,7 +6,7 @@ rem Build
 mkdir build && cd build || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Debug ^
-    -DCMAKE_CXX_FLAGS="" ^
+    -DCMAKE_CXX_FLAGS="/permissive-" ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
     -DUTILITY_USE_ANSI_COLORS=%ANSI_COLORS% ^
     -DBUILD_TESTS=ON ^
